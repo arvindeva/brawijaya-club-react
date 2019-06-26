8e9fc702abbd8995681ed04b1cd7742a29f32ff2
arvindeva <arvindeva@gmail.com>
2019-06-26 18:30:59 +0700

    Setup apollo, typescript

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import * as React from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
 
 type Props = {
-  title?: string
-}
+  title?: string;
+};
 
 const Layout: React.FunctionComponent<Props> = ({
   children,
-  title = 'This is the default title',
+  title = 'This is the default title'
 }) => (
   <div>
     <Head>
@@ -37,6 +37,6 @@ const Layout: React.FunctionComponent<Props> = ({
       <span>I'm here to stay (Footer)</span>
     </footer>
   </div>
-)
+);
 
-export default Layout
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,87 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import { NextPage } from 'next'
+import * as React from 'react';
+import Link from 'next/link';
+import { gql } from 'apollo-boost';
+import { NextPage } from 'next';
+import { Mutation, Query } from 'react-apollo';
+
+import Layout from '../components/Layout';
+
+const SIGNIN_MUTATION = gql`
+  mutation {
+    signIn(login: "arvindeva", password: "arvindeva") {
+      token
+    }
+  }
+`;
+
+const SIGNOUT_MUTATION = gql`
+  mutation {
+    signOut
+  }
+`;
+
+const ME_QUERY = gql`
+  query {
+    me {
+      id
+      username
+    }
+  }
+`;
+
+interface Data {
+  me: {
+    id: number;
+    username: string;
+  };
+}
 
 const IndexPage: NextPage = () => {
   return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
+    <Layout title="Home">
+      <h1>Hi 👋</h1>
       <p>
         <Link href="/about">
           <a>About</a>
         </Link>
       </p>
+      <Mutation
+        mutation={SIGNIN_MUTATION}
+        refetchQueries={[{ query: ME_QUERY }]}
+      >
+        {(signIn: any) => (
+          <button
+            onClick={async () => {
+              const response = await signIn();
+              console.log(response);
+            }}
+          >
+            Call login mutation
+          </button>
+        )}
+      </Mutation>
+      <Mutation
+        mutation={SIGNOUT_MUTATION}
+        refetchQueries={[{ query: ME_QUERY }]}
+      >
+        {(signOut: any) => (
+          <button
+            onClick={async () => {
+              const response = await signOut();
+              console.log(response);
+            }}
+          >
+            Call signout mutation
+          </button>
+        )}
+      </Mutation>
+      <Query<Data> query={ME_QUERY}>
+        {({ data }: { data: any }) => {
+          return <p>{data.me ? data.me.username : 'please sign in'}</p>;
+        }}
+      </Query>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
