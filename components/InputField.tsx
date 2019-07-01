3721f62718f6c50213f79acb28282d0c90e5ba6b
arvindeva <arvindeva@gmail.com>
2019-07-01 14:42:18 +0700

    Setup Formik

---
+++ components/InputField.tsx
@@ -0,0 +1,13 @@
+import React, { DetailedHTMLProps, InputHTMLAttributes } from 'react';
+import { FieldProps } from 'formik';
+
+type InputProps = DetailedHTMLProps<
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
+
+export const InputField = ({
+  field,
+  form: _,
+  ...props
+}: FieldProps & InputProps) => <input {...field} {...props} />;
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,10 +1,25 @@
-import * as React from 'react';
+import React from 'react';
+import { Formik, Field } from 'formik';
+
 import Layout from '../components/Layout';
+import { InputField } from '../components/InputField';
 
 const SignUpPage: React.FunctionComponent = () => (
   <Layout title="Sign Up">
-    <h1>Sign Up</h1>
-    <p>Please sign up</p>
+    <Formik
+      onSubmit={() => {}}
+      initialValues={{
+        email: '',
+        username: '',
+        password: '',
+      }}
+    >
+      {({ values, handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <Field name="email" component={InputField} />
+        </form>
+      )}
+    </Formik>
   </Layout>
 );
 
